50ad2322627bc910d83c3c9c598a6db86bc9df6a
pyeon ahyeon <piona9355@naver.com>
2022-03-10 10:15:38 +0900

    객체지향 프로그래밍 2주차 수업

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		/* 첫번째 예제 프로그램
+		 * 버전 1.8.0 2022,03.10
+		 * 편아현
+		 */
+		// 인사 출력
+		System.out.println("Hello, World!");
+		System.out.println("I'm a new java programer.");
+	}
+
+}
